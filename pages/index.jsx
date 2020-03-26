0a3e0dc99020064da987d4fadf7c47503bb51a7e
rhythm191 <sac2ndg+develop@gmail>
2020-03-26 17:52:12 +0900

    add products section

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,12 +8,12 @@ export default function Index() {
         <meta name="description" content="Rhyztech is tech team powered by rhythm191" />
       </Head>
 
-      <section>
+      <section id="hero-section">
         <h1>Rhyztech</h1>
         <p>powered by rhythm191</p>
       </section>
 
-      <section>
+      <section id="about">
         <h2>About</h2>
         <p>
           中の人はWebエンジニアです。名古屋のSIerでフルスタックエンジニアとしてWebサービスの開発を経験した後、
@@ -25,6 +25,20 @@ export default function Index() {
           自宅警備員時代は引きこもりながらReactとVueの修行をしていました。
           組織改革にも関心があって、新しいツールを導入したり、新しいプロセスを設計したりするのが好きです。
         </p>
+      </section>
+
+      <section id="products">
+        <h2>Products</h2>
+        <div>
+          <h3>Alfred-emmet</h3>
+          <p>Alfred上でEmmetの変換が使えるAlfred workflowです。</p>
+          <a href="https://www.npmjs.com/package/alfred-emmet">visit</a>
+        </div>
+        <div>
+          <h3>ルーン文字ジェネレータ</h3>
+          <p>ルーン文字の画像メーカーを作りました。</p>
+          <a href="https://rune-generator.firebaseapp.com/">visit</a>
+        </div>
       </section>
     </main>
   );
