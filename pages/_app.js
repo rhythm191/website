f2dda2cfced8acdddfa49de853fa925856440033
rhythm191 <sac2ndg+develop@gmail>
2020-03-29 09:27:36 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Router from "next/router";
+
+import * as gtag from "../lib/gtag";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/style.scss";
+
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
