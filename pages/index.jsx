1f6c51550553e3af12978eb8a62dc5dbce982e91
rhythm191 <sac2ndg+develop@gmail>
2020-03-29 09:27:52 +0900

    add google search console meta tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,10 @@ export default function Index() {
         <meta name="msapplication-TileColor" content="#0078d7" />
         <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
         <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content="BRJj_dard5y_F7XJG830_Wyo9f3npeybkl0wcG5z2tY"
+        />
       </Head>
 
       <section id="hero-section">
