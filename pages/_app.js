cc1248da35abde6a3c4536a62b371f8722c96cbd
rhythm191 <sac2ndg+develop@gmail.com>
2022-06-15 15:41:45 +0900

    feat: update google analytics components

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Router from "next/router";
-
-import * as gtag from "../lib/gtag";
+import { GoogleAnalytics } from "../components/GoogleAnalytics";
 
 import "../assets/style.scss";
 
-Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
